9c342bf7769bc9313d8407211777a7ea3da42b69
MrTroble <nifroh56@gmail.com>
2020-08-01 16:26:26 +0200

    Intermediate

--- TGTools/TGToolsConsole/TGTools.cpp
+++ TGTools/TGToolsConsole/TGTools.cpp
@@ -1,14 +1,26 @@
 ﻿// TGTools.cpp : Defines the entry point for the application.
 //
 
-#include "TGTools.h"
-#include <Texture.hpp>
+#include <iostream>
+#include <public/Texture.hpp>
 
 using namespace std;
 using namespace tgt;
 
+const std::string subcommands[] = { "map", "actor", "texture", "font" };
+
+typedef Result (*command)(int count, char**);
+const command commandhandle = {
+	[](int count, char** input) { return Result::SUCCESS; }
+};
+
 int main(int count, char** args)
 {
+	if (count == 0) {
+		// TODO console mode
+	} else {
+		constexpr size_t size = sizeof(subcommands) / sizeof(subcommands[0]);
+	}
 	auto result = Texture::add("");
 	cout << "Hello CMake. " << (int)result << endl;
 	return 0;
