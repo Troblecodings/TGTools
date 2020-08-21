6d597df576b3ea87f3d4d0ff291c29aa106a12a2
MrTroble <nifroh56@gmail.com>
2020-08-21 23:14:12 +0200

    Update Material.hpp

--- TGTools/TGTools/public/Material.hpp
+++ TGTools/TGTools/public/Material.hpp
@@ -11,8 +11,8 @@ namespace tgt::Material {
 
 	constexpr auto DEFAULT_COLOR = 0xFFFFFFFF;
 
-	constexpr auto TEXTURE_PROPERTY("texture");
-	constexpr auto COLOR_PROPERTY("color");
+	constexpr auto TEXTURE_PROPERTY = "texture";
+	constexpr auto COLOR_PROPERTY = "color";
 
 	constexpr std::array SUPPORTED_PROPERTIES = { TEXTURE_PROPERTY, COLOR_PROPERTY };
 
