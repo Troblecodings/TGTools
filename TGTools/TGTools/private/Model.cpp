9b2494a76232a5cd98569efadef3826048cb1ade
MrTroble <nifroh56@gmail.com>
2020-08-27 22:54:07 +0200

    Added GLTF laoder

---
+++ TGTools/TGTools/private/Model.cpp
@@ -0,0 +1,10 @@
+#include "../public/Model.hpp"
+
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "../../../tinygltf/tiny_gltf.h"
+
+namespace tgt::Model {
+
+}
