295c4ca84043d6318c596ee33a3b84c773f879e1
MrTroble <nifroh56@gmail.com>
2020-09-21 16:27:42 +0200

    Finished Sampler implementation

--- TGTools/TGTools/private/Sampler.cpp
+++ TGTools/TGTools/private/Sampler.cpp
@@ -2,15 +2,6 @@
 
 namespace tgt::Sampler {
 
-	constexpr auto SAMPLER_SUBFOLDER = "Sampler";
-
-	constexpr auto UMODE_PROPERTY = "textures";
-	constexpr auto VMODE_PROPERTY = "actors";
-	constexpr auto MAG_FILTER_PROPERTY = "materials";
-	constexpr auto MIN_FILTER_PROPERTY = "fonts";
-
-	const auto SAMPLER_PATH = fs::path(Util::RESOURCE_LOCATION).append(SAMPLER_SUBFOLDER);
-
 	const Result add(const std::string& name, const SamplerAddressMode umode, const SamplerAddressMode vmode,
 		const SamplerFilter magfilter, const SamplerFilter minFilter) {
 		STRING_CHECKS(name);
@@ -19,13 +10,16 @@ namespace tgt::Sampler {
 		ENUM_CHECKS(magfilter, SAMPLER_FILTER_MIN, SAMPLER_FILTER_MAX);
 		ENUM_CHECKS(minFilter, SAMPLER_FILTER_MIN, SAMPLER_FILTER_MAX);
 
+		auto path = Util::getResource(SAMPLER_PATH, name, Util::JSON);
+		if (fs::exists(path))
+			return Result::ALREADY_EXISTS;
+
 		js::json json;
 		json[UMODE_PROPERTY] = umode;
 		json[VMODE_PROPERTY] = vmode;
 		json[MAG_FILTER_PROPERTY] = magfilter;
 		json[MIN_FILTER_PROPERTY] = minFilter;
 
-		auto path = Util::getResource(SAMPLER_PATH, name, Util::JSON);
 		JSON_WRITE(path, json);
 	}
 
--- TGTools/TGTools/public/Sampler.hpp
+++ TGTools/TGTools/public/Sampler.hpp
@@ -5,6 +5,17 @@
 
 namespace tgt::Sampler {
 
+	constexpr auto SAMPLER_SUBFOLDER = "Sampler";
+
+	constexpr auto UMODE_PROPERTY = "u_address_mode";
+	constexpr auto VMODE_PROPERTY = "v_address_mode";
+	constexpr auto MAG_FILTER_PROPERTY = "magnification_filter";
+	constexpr auto MIN_FILTER_PROPERTY = "minification_filter";
+
+	const auto SAMPLER_PATH = fs::path(Util::RESOURCE_LOCATION).append(SAMPLER_SUBFOLDER);
+
+	constexpr std::array SUPPORTED_PROPERTIES = {UMODE_PROPERTY, VMODE_PROPERTY, MAG_FILTER_PROPERTY, MIN_FILTER_PROPERTY};
+
 	enum class SamplerAddressMode {
 		REPEAT = 0,
 		MIRRORED_REPEAT = 1,
@@ -30,4 +41,22 @@ namespace tgt::Sampler {
 	inline const Result add(const std::string& name, const SamplerAddressMode mode, const SamplerFilter filter) {
 		return add(name, mode, mode, filter, filter);
 	}
+
+	inline const Result remove(const std::string& name) {
+		STRING_CHECKS(name);
+
+		auto path = Util::getResource(SAMPLER_PATH, name, Util::JSON);
+		if (!fs::remove(path))
+			return Result::DOES_NOT_EXIST;
+		return Result::SUCCESS;
+	}
+
+	inline const std::string list() {
+		return Util::collect(SAMPLER_PATH, Util::JSON_FILTER);
+	}
+
+	inline const Result change(const std::string& name, const std::string& key, const std::string& value) {
+		return Util::change(Util::getResource(SAMPLER_PATH, name, Util::JSON), key, value, SUPPORTED_PROPERTIES);
+	}
+
 }
